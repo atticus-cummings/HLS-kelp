2cf3a16ecfdff185174e210777c8e31898ec9ebf
Atticus Cummings <atticuscummings@ucla.edu>
2024-09-26 17:52:42 -0700

    Add ability to specify download tile, removing need for geojson

--- tools/earth_access_tools.py
+++ tools/earth_access_tools.py
@@ -88,23 +88,90 @@ def create_tiling_reference(data_xml=r'C:\Users\attic\HLS Kelp Detection\maps\S2
     return csv_file
 
 
-def search_bounding_box(tile_id, csv_file=r'C:\Users\attic\HLS Kelp Detection\maps\tiling_boxes.csv'):
+def search_bounding_box(tile_id, csv_file=r'C:\Users\attic\HLS Kelp Detection\maps\tiling_boxes.csv', clip=False):
         with open(csv_file, mode='r') as file:
             csv_reader = csv.reader(file)
-            coordinates = None
+            coords = None
             for row in csv_reader:
                 if row[0] == tile_id:
                     coords = row[1:]
                     break
+            if coords is None:
+                return None
         bounding_box=(float(coords[9]), float(coords[10]), float(coords[3]),float(coords[4]))
         return bounding_box
 
 
 def download_dem (tile_id, dem_path): 
     bounding_box = search_bounding_box(tile_id)
+    if bounding_box is None:
+        print("Invalid tile")
+        return
     earthaccess.login(persist=True)
     dem_results = earthaccess.search_data(
         short_name="ASTGTM",
         bounding_box=bounding_box)
     for result in dem_results:
         earthaccess.download(result, local_path=dem_path)
+
+def download_hls_data(temporal=None,tiles=None,bbox=None,data_folder=r'C:\Users\attic\HLS Kelp Detection\imagery\tiles', num_download=500, num_load= -1, cloud_coverage=50):
+    # for reference, temporal input should be in this form: ("2018-12-25T00:00:00", "2019-01-01T00:00:00")
+    #
+    bbox_array = []
+    if tiles is not None:
+        for tile in tiles:
+            bbox_array.append(search_bounding_box(tile))
+    elif bbox is not None:
+        bbox_array.append(bbox)
+    if not bbox_array:
+        return False
+    earthaccess.login(persist=True)
+    downloaded = 0
+    for bounding_box in bbox_array:
+        print(bounding_box)
+        results = earthaccess.search_data(
+            short_name=['HLSL30','HLSS30'],
+            temporal=temporal,
+            count=load_num,
+            bounding_box=bounding_box
+        )
+        if results is None:
+            continue
+        for i, granule in enumerate(results):
+            metadata = extract_metadata(granule)
+            ## ======= Parse metadata ======= ##
+
+            name = metadata['native-id']
+
+            #For some reason, attributes are parsed into a list in the HLS metadata. This reformats it into a dictionary.
+            #print(attributes['MGRS_TILE_ID'])
+
+            if(int(metadata['CLOUD_COVERAGE']) > cloud_coverage): #Reject granules with large cloud cover, for now
+                continue
+            #time = metadata['SENSING_TIME']
+            tile_folder = metadata['MGRS_TILE_ID']
+            if(tiles is not None and tile_folder not in tiles):
+                continue
+            ## ======= Create file directory, if needed  ======= ##
+            tile_path = os.path.join(data_folder,tile_folder)
+            if not os.path.isdir(tile_path):
+                os.mkdir(tile_path)
+            folder_name = (f'{name}')
+            file_path = os.path.join(tile_path,folder_name)
+            if not os.path.isdir(file_path):
+                os.mkdir(file_path) #Make folder for granule 
+
+            # sorted data links 
+            all_links = granule.data_links()
+            links = get_band_links(all_links, name)
+
+            ## ======= download granule ======= ##
+            with open(os.devnull, 'w') as f, redirect_stdout(f): #The print out of this is kind of annoying, this redirects *most* of it 
+                downloadPath = earthaccess.download(links, local_path=file_path, threads=2)
+            downloaded = downloaded + 1
+            print(f'{name}')
+            if downloaded > num_download:
+                break
+            ## ======= write metadata csv ======= ##
+            save_metadata_csv(metadata,file_path,folder_name)
+    return True
